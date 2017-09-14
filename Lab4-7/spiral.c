d8426bcff4170c9e84d1e1fa1469f705ec5f8447
Ryan Sims <ryanjsims@email.arizona.edu>
2017-09-14 12:12:01 -0700

    Added SADness to spiral.c

--- Lab4-7/spiral.c
+++ Lab4-7/spiral.c
@@ -1,29 +1,55 @@
+void loops(int* size, int* frame, int* window) {
+	int minc = 0;
+	int maxc = 4 * (size[0] - 1); // compensate for window
+	int minr = 0;
+	int maxr = 4 * (size[1] - 1); //compensate for window
+	int len = size[0];
+	int c = 0;
+	int r = 0;
 
-minc = 0
-maxc = 4 * (len - 1) // compensate for window
-minr = 0
-maxr = 4 * (height - 1) //compensate for window
-c = 0
-r = 0
 
+	for (c; c < maxc; c += 4) { //right
+		address = (r*len + c);
+		val = frame[address];
+	}
+	minr += 4;
+	for (r; r < maxr; r += 4) { //down
+		address = (r*len + c);
+		val = frame[address];
+	}
+	maxc -= 4;
+	for (c; c > minc; c -= 4) { //left
+		address = (r*len + c);
+		val = frame[address];
+	}
+	maxr -= 4;
+	for (r; r > minr; r -= 4) { //up
+		address = (r*len + c);
+		val = frame[address];
+	}
+	minc += 4;
+}
 
-for(c = minc; c < maxc; c += 4){ //right
-	address = (r*len+c);
-	val = frame[address];
+int SAD(int* size, int* frameChunk, int* window) { //Frame chunk is address of top left corner of the chunk of the window we're looking at
+	int r = 0;
+	int c = 0;
+	int lenW = 4 * size[2];		//Window's length
+	int heightW = 4 * size[3];	//Window's height
+	int lenF = 4 * size[0];		//Frame length
+	int sum = 0;
+
+	for (r; r < heightW; r += 4) {
+		for (c = 0; c < lenW; c += 4) {
+			int diff = 0;
+			int addressWin = (r*lenW + c);	//Window has different length than the frame, so it needs its own address
+			int addressF = (r*lenF + c);	//Same with frame.
+			int valFrame = frameChunk[addressF];
+			int valWindow = window[addressWin];
+			diff = valFrame - valWindow;
+			if (diff < 0)			//Absolute value
+				diff = diff * -1;	//This can be done with either multi or two's complement
+			sum = sum + tempSum;
+		}
+	}
+	return sum;
 }
-minr += 4;
-for(r = minr; r < maxr; r += 4){ //down
-	address = (r*len + c);
-	val = frame[address];
-}
-maxc -=4;
-for(c; c > minc; c -= 4){ //left
-	address = (r*len + c);
-	val = frame[address];
-}
-maxr -= 4;
-for(r; r > minr; r -= 4){ //up
-	address = (r*len + c);
-	val = frame[address];
-}
-minc +=4;
