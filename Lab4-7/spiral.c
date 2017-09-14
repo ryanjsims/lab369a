a97c9c3240fcb65ad27d2386a0ed00d332fc65a2
Ryan Sims <ryanjsims@email.arizona.edu>
2017-09-13 18:16:40 -0700

    Added loops.s, modified spiral.c so no overlap occurs.

--- Lab4-7/spiral.c
+++ Lab4-7/spiral.c
@@ -7,19 +7,23 @@ c = 0
 r = 0
 
 
-for(c = minc ; c<=(maxc); c += 4){ //right
-  address = (r*len+c);
+for(c = minc; c < maxc; c += 4){ //right
+	address = (r*len+c);
+	val = frame[address];
 }
 minr += 4;
-for(r = minr ; r<= (maxr); r += 4){ //down
-
+for(r = minr; r < maxr; r += 4){ //down
+	address = (r*len + c);
+	val = frame[address];
 }
 maxc -=4;
-for(c; c >= minc; c -= 4){ //left
-
+for(c; c > minc; c -= 4){ //left
+	address = (r*len + c);
+	val = frame[address];
 }
-maxr -=4;
-for(r; r >= minr; r -= 4){ //up
-
+maxr -= 4;
+for(r; r > minr; r -= 4){ //up
+	address = (r*len + c);
+	val = frame[address];
 }
 minc +=4;
