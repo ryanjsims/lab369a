2ed371d1299d8f5218dfd95c8b97bb572dfb2ba6
Hardy <dennishardy@email.arizona.edu>
2017-09-18 14:26:48 -0700

    added spiral patern with test &fixed infinite loop

--- Lab4-7/vbsme.s
+++ Lab4-7/vbsme.s
@@ -498,7 +498,7 @@ main:
     la      $a2, window1    # 3rd parameter: address of window1[0]
 
     jal     vbsme           # call function
-    jal     print_result    # print results to console
+    #jal     print_result    # print results to console
 
     ############################################################
     # End of test 1
@@ -778,5 +778,107 @@ vbsme:
     li      $v1, 0
 
     lw      $t0, 1020($a1)
-    jr      $ra
+    #jr      $ra
     # insert your code here
+loop:
+		li		$t0, 0			#minc
+		li		$t2, 0			#minr
+
+		#asize: i, j, k, l, located in $a0
+		#aframe: located in $a1
+		#awindow: located in $a2
+
+		lw		$t4, 0($a0)		#load length
+		addi	$t1, $t4, -1	#length - 1
+		sll		$t1, $t1, 2		#4 * (length - 1) = maxc
+
+		lw		$t5, 4($a0)		#load height
+		addi	$t3, $t5, -1	#height - 1
+		sll		$t3, $t3, 2		#4 * (height - 1) = maxr
+		
+		add		$s0, $0, $0		#col = 0
+		add		$s1, $0, $0		#row = 0
+
+spiral:
+right:	
+		mult	$s1, $t4		#row * len
+		mflo    $s2
+		add		$s2, $s2, $s0	#address = row * len + col
+		add		$s2, $s2, $a1	#get address of val in aframe
+		lw		$v0, 0($s2)		#val = frame[address]
+		addi	$sp, $sp, -4
+		sw		$ra, 0($sp)
+		jal		printVal
+		lw		$ra, 0($sp)
+		addi	$sp, $sp, 4
+		addi	$s0, $s0, 4		#c += 4
+		slt		$t7, $s0, $t1	#c < maxc
+		bne		$t7, $0, right  #if(c == maxc) break;
+
+		addi	$t2, $t2, 4		#minr += 4
+
+down:	mult	$s1, $t4		#row * len
+		mflo    $s2
+		add		$s2, $s2, $s0	#address = row * len + col
+		add		$s2, $s2, $a1
+		lw		$v0, 0($s2)
+		addi	$sp, $sp, -4
+		sw		$ra, 0($sp)
+		jal		printVal
+		lw		$ra, 0($sp)
+		addi	$sp, $sp, 4
+		addi	$s1, $s1, 4		#row += 4
+		slt		$t7, $s1, $t3	#row < maxr
+		bne		$t7, $0, down	#if(r == maxr) break;
+
+		addi	$t1, $t1, -4	#maxc -= 4;
+
+left:	mult	$s1, $t4		#row * len
+		mflo    $s2
+		add		$s2, $s2, $s0	#address = row * len + col
+		add		$s2, $s2, $a1
+		lw		$v0, 0($s2)
+		addi	$sp, $sp, -4
+		sw		$ra, 0($sp)
+		jal		printVal
+		lw		$ra, 0($sp)
+		addi	$sp, $sp, 4
+		addi	$s0, $s0, -4
+		slt		$t7, $t0, $s0	#minc < c
+		bne		$t7, $0, left	#if(minc < c) break;
+
+		addi	$t3, $t3, -4	#maxr -= 4;
+
+up:		mult	$s1, $t4		#row * len
+		mflo    $s2
+		add		$s2, $s2, $s0	#address = row * len + col
+		add		$s2, $s2, $a1
+		lw		$v0, 0($s2)
+		addi	$sp, $sp, -4
+		sw		$ra, 0($sp)
+		jal		printVal
+		lw		$ra, 0($sp)
+		addi	$sp, $sp, 4
+		addi	$s1, $s1, -4
+		slt		$t7, $t2, $s1	#minr < r
+		bne		$t7, $0, up		#if(minr < r) break;
+
+		addi	$t0, $t0, 4
+		slt		$t7, $t0, $t1	#loop if minc < maxc
+		bne     $t7, $0, spiral
+		
+		slt		$t7, $t2, $t3	#loop if minr < maxr
+		bne		$t7, $0, spiral	
+
+		jr		$ra				#return
+		
+printVal:
+		add     $a0, $v0, $zero     # Load $v0 for printing 
+		li      $v0, 1              # Load the system call numbers
+        syscall
+
+		# Print newline.
+		la      $a0, newline          # Load value for printing
+		li      $v0, 4                # Load the system call numbers
+		syscall
+		jr $ra
