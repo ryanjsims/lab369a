9887f91055697f5b8522f68da304efdfa0fec9ca
Hardy <dennishardy@email.arizona.edu>
2017-09-20 15:12:38 -0700

    Finished sad function but it doesnt work

--- Lab4-7/SAD.s
+++ Lab4-7/SAD.s
@@ -1,25 +1,45 @@
+SAD:
+		lw $s3, 8($a0)  #$s3 = window length
+		lw $s4, 12($a0) #$s4 = window height
 
+		#$s2 has adress of top left pixel to be checked
+		#$a2 has adress of top left of window
 
-	lw $s3, 8($a0)  #$s3 = window length
-	lw $s4, 12($a0) #$s4 = window height
-	lw $s5, 0($a0)	#$s5 = frame length
-
-	#$s2 has adress of top left pixel to be checked
-	#$a2 has adress of top left of window
-
-	addi $s6, $s2, 0
-	addi $s7, $a2, 0
-	add $t8, $0, $0
+		addi $s6, $s2, 0 #s6 = address of top left pixel to be checked
+		addi $s7, $a2, 0 #s7 = address of first element in window
+		add $t8, $0, $0  #t8 = sum
 	
-loop1:
-	lw $t6, 0($s6)
-	lw $t7, 0($s7)
-	sub $t6, $t6, $t7
-	slt $t7, $t6, $0
-	bne $t7, $0, Sum
-	sub $t6, $0, $t6
+		sll $s3, $s3, 2 #s3 = 4 * window length
+		addi $t6, $s3, -4
+		mult $t6, $s4
+		mflo $s5       #s5 = very last adress to be used
+		sll $s4, $t4, 2
+		sub $s4, $s4, $s3
+	
+newRow:
+		
+		add $t9, $s3, $s7 #s3 = addrress of last element in row bieng checked
+		
+		lw $t6, 0($s6)
+		lw $t7, 0($s7)
+		sub $t6, $t6, $t7
+		slt $t7, $t6, $0
+		bne $t7, $0, Sum
+		sub $t6, $0, $t6
 Sum:
-	add $t8, $t8, $t6
-	
-	#move pixel through window
-	#repeat
+		add $t8, $t8, $t6
+		
+		bne $s7, $t9, nextPixel
+		add $s6, $s6, $s4 #move down a row
+		add $t9, $s3, $s7 #s3 = addrress of last element in row bieng checked
+		addi $t9, $t9, 4
+		bne $s7, $s5, nextPixel
+		addi $v0, $t8, 0  #return sum
+		jr $ra
+		
+nextPixel:
+		addi $s6, $s6, 4
+		addi $s7, $s7, 4
+		
+		j newRow
+		
--- Lab4-7/vbsme.s
+++ Lab4-7/vbsme.s
@@ -807,9 +807,9 @@ right:
 		mflo    $s2
 		add		$s2, $s2, $s0	#address = row * len + col
 		add		$s2, $s2, $a1	#get address of val in aframe
-		lw		$v0, 0($s2)		#val = frame[address]
 		addi	$sp, $sp, -4
 		sw		$ra, 0($sp)
+		jal		SAD
 		jal		printVal
 		lw		$ra, 0($sp)
 		addi	$sp, $sp, 4
@@ -823,9 +823,9 @@ down:	mult	$s1, $t4		#row * len
 		mflo    $s2
 		add		$s2, $s2, $s0	#address = row * len + col
 		add		$s2, $s2, $a1
-		lw		$v0, 0($s2)
 		addi	$sp, $sp, -4
 		sw		$ra, 0($sp)
+		jal		SAD
 		jal		printVal
 		lw		$ra, 0($sp)
 		addi	$sp, $sp, 4
@@ -839,9 +839,9 @@ left:	mult	$s1, $t4		#row * len
 		mflo    $s2
 		add		$s2, $s2, $s0	#address = row * len + col
 		add		$s2, $s2, $a1
-		lw		$v0, 0($s2)
 		addi	$sp, $sp, -4
 		sw		$ra, 0($sp)
+		jal		SAD
 		jal		printVal
 		lw		$ra, 0($sp)
 		addi	$sp, $sp, 4
@@ -855,9 +855,9 @@ up:		mult	$s1, $t4		#row * len
 		mflo    $s2
 		add		$s2, $s2, $s0	#address = row * len + col
 		add		$s2, $s2, $a1
-		lw		$v0, 0($s2)
 		addi	$sp, $sp, -4
 		sw		$ra, 0($sp)
+		jal		SAD
 		jal		printVal
 		lw		$ra, 0($sp)
 		addi	$sp, $sp, 4
@@ -884,3 +884,49 @@ printVal:
 		li      $v0, 4                # Load the system call numbers
 		syscall
 		jr $ra
+
+SAD:
+		lw $s3, 8($a0)  #$s3 = window length
+		lw $s4, 12($a0) #$s4 = window height
+
+		#$s2 has adress of top left pixel to be checked
+		#$a2 has adress of top left of window
+
+		addi $s6, $s2, 0 #s6 = address of top left pixel to be checked
+		addi $s7, $a2, 0 #s7 = address of first element in window
+		add $t8, $0, $0  #t8 = sum
+	
+		sll $s3, $s3, 2 #s3 = 4 * window length
+		addi $t6, $s3, -4
+		mult $t6, $s4
+		mflo $s5       #s5 = very last adress to be used
+		sll $s4, $t4, 2
+		sub $s4, $s4, $s3
+	
+newRow:
+		
+		add $t9, $s3, $s7 #s3 = addrress of last element in row bieng checked
+		
+		lw $t6, 0($s6)
+		lw $t7, 0($s7)
+		sub $t6, $t6, $t7
+		slt $t7, $t6, $0
+		bne $t7, $0, Sum
+		sub $t6, $0, $t6
+Sum:
+		add $t8, $t8, $t6
+		
+		bne $s7, $t9, nextPixel
+		add $s6, $s6, $s4 #move down a row
+		add $t9, $s3, $s7 #s3 = addrress of last element in row bieng checked
+		addi $t9, $t9, 4
+		bne $s7, $s5, nextPixel
+		addi $v0, $t8, 0  #return sum
+		jr $ra
+		
+nextPixel:
+		addi $s6, $s6, 4
+		addi $s7, $s7, 4
+		
+		j newRow
+		
